cd0fa2061f39efdcfea9a12ae890b0ecd9b8b16c
somsekhardash <dashsomsekhar@gmail.com>
2022-02-15 15:00:13 +0530

    auth

--- src/Auth/RequireAuth.tsx
+++ src/Auth/RequireAuth.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {useAuth} from '../Layout';
 import {
     useLocation,
     Navigate
   } from "react-router-dom";
+import { UserAuthenticationClient } from '../Helper/Cookieshelper';
+import { REACT_APP_COOKIE3 } from '../config';
 
 function RequireAuth({ children }: { children: JSX.Element }) {
     let auth = useAuth();
@@ -11,7 +13,9 @@ function RequireAuth({ children }: { children: JSX.Element }) {
     debugger;
     console.log('---------------');
     console.log(auth);
-    if (!auth.user) {
+    const internalCookie = UserAuthenticationClient.getDocumentCookie(`${REACT_APP_COOKIE3}`);
+
+    if (!internalCookie) {
       // Redirect them to the /login page, but save the current location they were
       // trying to go to when they were redirected. This allows us to send them
       // along to that page after they login, which is a nicer user experience
